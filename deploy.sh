123bd75ce1b9624cce34c114c4a9dbfba65b69a7
Axel Monroy <xaxelmonroyx@gmail.com>
2019-11-05 15:31:53 -0700

    Change creation of certificate for instances

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-ssh-keygen -f $PWD/key_access_asg_nginx_example
 mkdir -p private/ssh ~/ssh_custom_keys
-mv $PWD/key_access_asg_nginx_example.pub $PWD/private/ssh/key_access_asg_nginx_example.pub
-mv $PWD/key_access_asg_nginx_example ~/ssh_custom_keys/key_access_asg_nginx_example.pem
-chmod 400 ~/ssh_custom_keys/key_access_asg_nginx_example.pem
-ssh-add ~/ssh_custom_keys/key_access_asg_nginx_example.pem # To allow agent forwarding
+ssh-keygen -f $PWD/private/ssh/key_access_asg_nginx_example -N ""
+
+mv $PWD/private/ssh/key_access_asg_nginx_example $PWD/private/ssh/key_access_asg_nginx_example.pem
+chmod 400 $PWD/private/ssh/key_access_asg_nginx_example.pem
+ssh-add $PWD/private/ssh/key_access_asg_nginx_example.pem # To allow agent forwarding
 
 cd environments/dev_nginx_elb/
 terraform init
