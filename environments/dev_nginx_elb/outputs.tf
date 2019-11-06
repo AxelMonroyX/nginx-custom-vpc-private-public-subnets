8c1e9018e4b3b79e041f2a3f28daebc4a9f13126
Axel Monroy <xaxelmonroyx@gmail.com>
2019-11-05 17:39:44 -0700

    Add output for dns name

--- environments/dev_nginx_elb/outputs.tf
+++ environments/dev_nginx_elb/outputs.tf
@@ -1,18 +1,4 @@
-# output "dns_name" {
-#   value = "${module.asg_example_elb.dns_name}"
-#   description = "Enter to this DNS to access the webpage"
-# }
-
-# output "my_ip" {
-#   value = "${chomp(data.http.my_ip.body)}/32"
-#   description = "IP for access to SSH"
-# }
-
-# output "ami_ubuntu_18_04" {
-#   value       = "${module.amis.ami_ubuntu_18_04}"
-#   description = "AMI used for the instances"
-# }
-
-output "aws_availability_zones" {
-  value = "${data.aws_availability_zones.available.names[0]}"
+output "dns_name" {
+  value = "${module.asg_example_elb.dns_name}"
+  description = "Enter to this DNS to access the webpage"
 }
